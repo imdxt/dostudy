3327d88eb2b7a6a5d72a446d55055c5906a21b2f
leo <leo@163.com>
2020-05-14 22:08:43 +0800

    添加解题思路说明
    Signed-off-by: leo <leo@163.com>

--- src/main/java/test/SingleNumber.java
+++ src/main/java/test/SingleNumber.java
@@ -12,6 +12,14 @@ import java.util.*;
  *
  * 输入: [2,2,1]
  * 输出: 1
+ * 解题思路：
+ *      1、如果不考虑时间复杂度和额外空间的话 使用集合存储遍历的数字，如果集合中没有则添加，如果集合中有则删除，最后集合中只剩下一个只出现一次的数字
+ *      2、位运算-（看了答案才明白的）
+ *  正确解法是使用异或运算
+ *      1、任何数和0做异或运算，结果仍然是原来的数，即 a⊕0=a。5^0 = 5
+ *      2、任何数和自身异或运算，结果是0。 5^5 = 0
+ *      3、异或运算满足交换定律和结合定律，即a⊕b⊕a=b⊕a⊕a=b⊕(a⊕a)=b⊕0=b。
+ *        5^3^5^6^7^6^7 = 3
  */
 public class SingleNumber {
     public static void main(String[] args){
@@ -27,7 +35,6 @@ public class SingleNumber {
             single ^=num;
         }
 
-        ArrayList
         return single;
     }
     private static int singleNumber(int[] num){
